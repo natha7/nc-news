d6b162d437bd713742c8f8b547e6e9a81c9a829c
Nathan A <nathaff7@gmail.com>
2024-10-14 12:15:44 +0100

    ticket-3 get endpoints

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -3,10 +3,22 @@ const seed = require("../db/seeds/seed.js");
 const testData = require("../db/data/test-data/index.js");
 const db = require("../db/connection.js");
 const app = require("../app.js");
+const endpoints = require("../endpoints.json");
 
 beforeEach(() => seed(testData));
 
 afterAll(() => db.end());
+
+describe("GET: /api", () => {
+  test("GET 200: Returns a JSON string describing the endpoints, acceptable queries, the format of the request body and an example response", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.endpoints).toEqual(endpoints);
+      });
+  });
+});
 
 describe("GET: /api/topics", () => {
   test("GET 200: Returns an array of topic objects which have the properties 'slug' and 'description'", () => {
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const { getEndpoints } = require("./controllers/getEndpoints.controller.js");
 const { getAllTopics } = require("./controllers/topics.controller");
 const { handleServerErrors } = require("./controllers/errors/index.js");
 
 const app = express();
+
+app.get("/api", getEndpoints);
 
 app.get("/api/topics", getAllTopics);
 
