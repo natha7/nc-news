ecfaf5553e9e5eb5ca22302f8c4f7fe6e9f66992
Nathan A <nathaff7@gmail.com>
2024-10-14 14:54:48 +0100

    ticket 5 - get articles

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -28,8 +28,8 @@ describe("GET: /api/topics", () => {
       .then(({ body }) => {
         expect(Array.isArray(body.topics)).toBe(true);
         body.topics.forEach((topic) => {
-          expect(topic.hasOwnProperty("slug")).toBe(true);
-          expect(topic.hasOwnProperty("description")).toBe(true);
+          expect(topic).toHaveProperty("slug");
+          expect(topic).toHaveProperty("description");
         });
       });
   });
@@ -41,20 +41,21 @@ describe("GET: /api/articles/:article_id", () => {
       .get("/api/articles/2")
       .expect(200)
       .then(({ body }) => {
+        const article = body.article;
         expect(body.article.article_id).toBe(2);
-        expect(body.article.hasOwnProperty("author")).toBe(true);
-        expect(body.article.hasOwnProperty("title")).toBe(true);
-        expect(body.article.hasOwnProperty("article_id")).toBe(true);
-        expect(body.article.hasOwnProperty("body")).toBe(true);
-        expect(body.article.hasOwnProperty("topic")).toBe(true);
-        expect(body.article.hasOwnProperty("created_at")).toBe(true);
-        expect(body.article.hasOwnProperty("votes")).toBe(true);
-        expect(body.article.hasOwnProperty("article_img_url")).toBe(true);
+        expect(article).toHaveProperty("body");
+        expect(article).toHaveProperty("author");
+        expect(article).toHaveProperty("article_id");
+        expect(article).toHaveProperty("topic");
+        expect(article).toHaveProperty("created_at");
+        expect(article).toHaveProperty("votes");
+        expect(article).toHaveProperty("article_img_url");
+        expect(article).toHaveProperty("title");
       });
   });
   test("GET 400: Returns a bad request error when article_id is not valid to process", () => {
     return request(app)
-      .get("/api/articles/an_author")
+      .get("/api/articles/invalid_id")
       .expect(400)
       .then(({ body }) => {
         expect(body.msg).toBe("Bad request");
@@ -66,6 +67,46 @@ describe("GET: /api/articles/:article_id", () => {
       .expect(404)
       .then(({ body }) => {
         expect(body.msg).toBe("Author does not exist");
+      });
+  });
+});
+
+describe("GET: /api/articles", () => {
+  test("GET 200: Returns an array of articles with each contained object having the correct properties, by default in descending order and no body property", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        const articles = body.articles;
+        expect(Array.isArray(articles)).toBe(true);
+        expect(articles).toBeSortedBy("created_at", { descending: true });
+        expect(
+          articles.find(
+            (article) => article.title === "Living in the shadow of a great man"
+          ).comment_count
+        ).toBe(11);
+        articles.forEach((article) => {
+          expect(article).not.toHaveProperty("body");
+          expect(article).toHaveProperty("author");
+          expect(article).toHaveProperty("article_id");
+          expect(article).toHaveProperty("topic");
+          expect(article).toHaveProperty("created_at");
+          expect(article).toHaveProperty("votes");
+          expect(article).toHaveProperty("article_img_url");
+          expect(article).toHaveProperty("comment_count");
+          expect(article).toHaveProperty("title");
+        });
+      });
+  });
+  test("GET 404: Sends a not found error if no articles are found", async () => {
+    await db.query(
+      `DELETE FROM comments; DELETE FROM articles; DELETE FROM users; DELETE FROM topics;`
+    );
+    return request(app)
+      .get("/api/articles")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("No articles found");
       });
   });
 });
