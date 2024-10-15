25382d25dd48ccc71450d071db0e693011983b53
Nathan A <nathaff7@gmail.com>
2024-10-15 16:00:22 +0100

    added queries for /api/articles

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,4 +1,5 @@
 const db = require("../db/connection.js");
+const format = require("pg-format");
 
 exports.fetchArticleById = (id) => {
   return db
@@ -11,20 +12,41 @@ exports.fetchArticleById = (id) => {
     });
 };
 
-exports.fetchArticles = () => {
-  return db
-    .query(
-      `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, CAST(COUNT(comments.body) AS INTEGER) AS comment_count FROM articles
-      JOIN comments ON articles.article_id = comments.article_id
-      GROUP BY articles.article_id
-      ORDER BY articles.created_at DESC`
-    )
-    .then(({ rows }) => {
-      if (rows.length === 0) {
-        return Promise.reject({ status: 404, msg: "No articles found" });
-      }
-      return rows;
-    });
+exports.fetchArticles = (sort_by = "created_at", order = "DESC") => {
+  order = order.toUpperCase();
+  sort_by = sort_by.toLowerCase();
+
+  const validQueries = [
+    "article_id",
+    "author",
+    "topic",
+    "created_at",
+    "votes",
+    "comment_count",
+    "title",
+  ];
+  const validOrders = ["DESC", "ASC"];
+
+  const queryValues = [];
+  let queryString = `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, CAST(COUNT(comments.body) AS INTEGER) AS comment_count FROM articles 
+  JOIN comments ON articles.article_id = comments.article_id 
+  GROUP BY articles.article_id
+  ORDER BY`;
+
+  if (validQueries.includes(sort_by) && validOrders.includes(order)) {
+    queryString += ` %I %s`;
+    queryValues.push(sort_by);
+    queryValues.push(order);
+  } else {
+    return Promise.reject({ status: 400, msg: "Bad request" });
+  }
+
+  return db.query(format(queryString, ...queryValues)).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "No articles found" });
+    }
+    return rows;
+  });
 };
 
 exports.fetchCommentsByArticleId = (id) => {
