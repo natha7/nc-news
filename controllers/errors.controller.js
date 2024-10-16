9e10ae55c66ac35bce626d699bacea3dce26775a
Nathan A <nathaff7@gmail.com>
2024-10-16 10:44:17 +0100

    added status to errors

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -1,21 +1,23 @@
 exports.handlePsqlErrors = (err, request, response, next) => {
   if (err.code === "22P02") {
-    response.status(400).send({ msg: "Bad request - invalid type" });
+    response
+      .status(400)
+      .send({ status: 400, msg: "Bad request - invalid type" });
   }
   if (err.code === "23503") {
-    response.status(404).send({ msg: "Username does not exist" });
+    response.status(404).send({ status: 404, msg: "Username does not exist" });
   }
   if (err.code === "23502") {
-    response.status(400).send({ msg: "Missing required key(s)" });
+    response.status(400).send({ status: 400, msg: "Missing required key(s)" });
   } else next(err);
 };
 
 exports.handleCustomErrors = (err, request, response, next) => {
   if (err) {
-    response.status(err.status).send({ msg: err.msg });
+    response.status(err.status).send({ status: err.status, msg: err.msg });
   } else next();
 };
 
 exports.handleServerErrors = (err, request, response, next) => {
-  response.status(500).send({ msg: "Internal Server Error" });
+  response.status(500).send({ status: 500, msg: "Internal Server Error" });
 };
